3aaec2e25953e648836343fc655c7323f6276c3b
Lilia <lilia.benhadjmabrouk@gmail.com>
2020-10-24 13:53:26 +0100

    link fragment with viewModel and with adapter

--- app/src/main/java/tn/thinkit/challenge/ui/fragments/HomeFragment.kt
+++ app/src/main/java/tn/thinkit/challenge/ui/fragments/HomeFragment.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package tn.thinkit.challenge.ui.fragments
 
 import android.os.Bundle
@@ -5,13 +7,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.home_fragment.*
 import tn.thinkit.challenge.R
+import tn.thinkit.challenge.data.model.Room
+import tn.thinkit.challenge.ui.adapters.RoomAdapter
+import tn.thinkit.challenge.ui.viewmodels.HomeViewModel
+import tn.thinkit.challenge.utilities.InjectorUtils
+import androidx.lifecycle.ViewModelProviders
 
-class HomeFragment : Fragment(){
+class HomeFragment : Fragment(), RoomAdapter.RoomListener{
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getRoomsList()
+    }
+
+    private fun getRoomsList() {
+        val factory = InjectorUtils.provideHomeViewModelFactory()
+        val viewModel = ViewModelProviders.of(this, factory).get(HomeViewModel::class.java)
+        listRooms.apply {
+            val mLayoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            layoutManager = mLayoutManager
+            val roomList = viewModel.getRooms().value!!
+            val roomAdapter =
+                RoomAdapter(
+                    roomList,
+                    this@HomeFragment,
+                    requireActivity()
+                )
+            adapter = roomAdapter
+        }
+    }
+
+    override fun onRoomClicked(room: Room, view: View) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/tn/thinkit/challenge/ui/fragments/MainFragment.kt
+++ app/src/main/java/tn/thinkit/challenge/ui/fragments/MainFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.main_fragment.*
 import tn.thinkit.challenge.R
 
 class MainFragment : Fragment(){
@@ -13,5 +15,16 @@ class MainFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        continueClick()
+    }
+
+    private fun continueClick(){
+        continueBtn.setOnClickListener {
+            findNavController().navigate(R.id.toHome)
+        }
     }
 }
